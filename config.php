9f2c50f109f45344f342e80bd32aa5c81017f579
thaint <thaint@blueseed.tv>
2018-08-03 15:15:09 +0700

    init project php RESTFul API

---
+++ config.php
@@ -0,0 +1,2 @@
+<?php
+$acces_token_config = 'aaaa';
---
+++ read_one.php
@@ -0,0 +1,47 @@
+<?php
+require 'vendor/autoload.php';
+require_once 'config.php';
+// Using Medoo namespace
+use Medoo\Medoo;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+if (isset($_GET['database']) && isset($_GET['keyword']) && isset($_GET['acces_token'])) { //Checks if cookies value exists
+    $database = $_GET["database"];
+    $keyword = $_GET["keyword"];
+    $acces_token = $_GET["acces_token"];
+
+    if ($acces_token === $acces_token_config) {
+	    $limit = $_GET["limit"];
+	    getKeyWords($database, $keyword, $limit);
+    } else {
+    	echo json_encode(array('Message' => 'Wrong Access Token!'));
+    }
+}
+
+function getKeyWords($database, $keyword, $limit) {
+	// MYSQL
+	define('DB_HOST', '127.0.0.1');
+	define('DB_PORT', '3306');
+	// define('DB_NAME', 'image');
+	define('DB_USER', 'root');
+	define('DB_PASSWORD', 'admin');
+
+	// Initialize
+	$DB = new Medoo([
+	    'database_type' => 'mysql',
+	    'database_name' => $database,
+	    'server' => DB_HOST,
+	    'port' => DB_PORT,
+	    'username' => DB_USER,
+	    'password' => DB_PASSWORD
+	]);
+	$platforms = $DB->select("keyword", [
+		"keyword"
+	], [
+		"keyword[~]" => $keyword,
+		"LIMIT" => $limit
+	]);
+	echo json_encode($platforms);
+}
+
